c67492daa9a68e5d568843f33cd57756de6c3fd9
Brandon De Leon <68029498+bdleon@users.noreply.github.com>
2022-06-08 13:32:17 -0500

    added should click element test

--- test/specs/elementActions.test.js
+++ test/specs/elementActions.test.js
@@ -1,5 +1,12 @@
- internetPage = require("../pageobjects/internet.page");
 
-describe("Test element actions", function(){
-    
+
+internetPage = require("../pageobjects/internet.page");
+
+describe("Test element actions", function () {
+
+    it('should click element', async () => {
+        await browser.url('/');
+        await internetPage.clickOnLink();
+        expect(await browser.getUrl()).equals('http://the-internet.herokuapp.com/abtest');
+    })
 })
