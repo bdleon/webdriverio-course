facf0a0f2b1b98a3b43aaf372407cfa671ba5c0c
Brandon De Leon <68029498+bdleon@users.noreply.github.com>
2022-06-09 15:13:36 -0500

    added two methods to enter username and password

--- test/pageobjects/internet.page.js
+++ test/pageobjects/internet.page.js
@@ -1,15 +1,52 @@
+const { default: $ } = require("webdriverio/build/commands/browser/$")
+
 
 
 class Internet {
 
     get pageHeader() { return $('h1.heading') }
     get subHeading() { return $('h2') }
+    get h3Header() { return $('h3')}
     get pageFooter() { return $('#page-footer') }
     get parent() { return $('ul') }
     get childElements() { return this.parent.$$('li') }
-
     specificChildElement(index) { return this.parent.$(`li:nth-child(${index})`) }
     get firstLink() { return $('ul li:nth-child(1) a') }
+    link(index) { return $(`ul li:nth-child(${index}) a`)}
+
+    checkBoxes(index) { return $(`#checkboxes input:nth-child(${index})`) }
+    get username() { return $('#username') }
+    get password() {return $('#password') }
+    /**
+     * inputs username to the field 
+     * @param {string} text username to be entered
+     */
+    async enterUsername(text) {
+        await this.username.waitForDisplayed()
+        await this.username.setValue(text)
+    }
+    /**
+     * Inputs password to the input field
+     * @param {string} text password to be entered
+     */
+    async enterPassword(text) {
+        await this.password.waitForDisplayed()
+        await this.password.setValue(text)
+    }
+
+    /**
+     * Clicks on the link based on the index provided
+     * @param {Number} index the index of the element
+     */
+    async clickLink(index){
+        await this.link(index).waitForDisplayed()
+        await this.link(index).click()
+    }
+    
+    async clickCheckBox(index) {
+        await this.checkBoxes(index).waitForDisplayed()
+        await this.checkBoxes(index).click()
+    }
 
     async getLiText() {
         await this.childElements.filter((e) => {
@@ -29,8 +66,7 @@ class Internet {
         if (await this.firstLink.isDisplayed() === true) {
             await this.firstLink.click()
         }
-        await browser.pause(5000)
-
+        await this.h3Header.waitForDisplayed()
     }
 
 }
--- test/specs/elementActions.test.js
+++ test/specs/elementActions.test.js
@@ -11,9 +11,21 @@ describe("Test element actions", function () {
         expect(await browser.getUrl()).equals('http://the-internet.herokuapp.com/abtest');
     })
 
-    it('should get text', async ()=>{
+    it('should get text', async () => {
         await browser.url('/');
         expect(await internetPage.getSpecificElementText(1)).equals('A/B Testing');
 
+    })
+
+    it('should click checkbox', async () => {
+        await internetPage.clickLink(6);
+        await internetPage.clickCheckBox(1);
+        expect(await internetPage.checkBoxes(1).isSelected()).equals(true);
+    })
+
+    it('should unclick checkbox', async () => {
+        
+        await internetPage.clickCheckBox(1);
+        expect(await internetPage.checkBoxes(1).isSelected()).equals(false);
     })
 })
