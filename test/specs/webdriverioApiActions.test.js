00c1356620fe715f003331e7b8f9484d52bb7251
Brandon De Leon <68029498+bdleon@users.noreply.github.com>
2022-06-10 15:40:17 -0500

    added tests and methods for grabbing text from figure

--- test/specs/webdriverioApiActions.test.js
+++ test/specs/webdriverioApiActions.test.js
@@ -2,11 +2,23 @@
 
  internetPage = require("../pageobjects/internet.page");
 
-describe('WebdriverIO API Actions', function (){
-    it('Should hover on figure', async () => {
+describe('WebdriverIO API Actions', function () {
+    it('Should hover on figure 1', async () => {
         await browser.url("/hovers");
         await internetPage.hoverOverFigure(3);
         assert.equal("name: user1", await internetPage.getFigureDetailsText(3),"Did not match figure text");
+        
+    })
+    it('Should hover on figure 2', async () => {
+        await browser.url("/hovers");
+        await internetPage.hoverOverFigure(4);
+        assert.equal("name: user2", await internetPage.getFigureDetailsText(4),"Did not match figure text");
+        
+    })
+    it('Should hover on figure 3', async () => {
+        await browser.url("/hovers");
+        await internetPage.hoverOverFigure(5);
+        assert.equal("name: user3", await internetPage.getFigureDetailsText(5),"Did not match figure text");
         
     })
 })
