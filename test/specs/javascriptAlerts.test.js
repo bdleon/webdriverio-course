9266abd6ef9b332addc94c899818633473ee13df
Brandon De Leon <68029498+bdleon@users.noreply.github.com>
2022-06-13 15:30:12 -0500

    added 2 more test; should dismiss alert, and should sent text to alert

--- test/specs/javascriptAlerts.test.js
+++ test/specs/javascriptAlerts.test.js
@@ -4,16 +4,27 @@ describe('Javascript Alerts', function () {
     it('Should get text of alert', async () => {
         await browser.url('/javascript_alerts');
         await internetPage.clickJavascriptAlertButton(1);
-        assert.equal('I am a JS Alert', await browser.getAlertText())
-
-
-
+        assert.equal('I am a JS Alert', await browser.getAlertText());
     })
 
     it('should accept alert', async () => {
         await browser.acceptAlert();
+        assert.equal('You successfully clicked an alert', await internetPage.getResutsText());
        
 
+    })
+    it('should dismiss alert', async ()=>{
+        await internetPage.clickJavascriptAlertButton(2);
+        await browser.dismissAlert();
+        assert.equal('You clicked: Cancel', await internetPage.getResutsText());
+
+    })
+
+    it('Should send text to alert', async ()=>{
+        await internetPage.clickJavascriptAlertButton(3);
+        await browser.sendAlertText('This is some text');
+        await browser.acceptAlert();
+        assert.equal('You entered: This is some text', await internetPage.getResutsText())
     })
 })
 
