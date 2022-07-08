5c0c6744cbfb6e7376e1cfa4b262ecb0501be513
Brandon De Leon <68029498+bdleon@users.noreply.github.com>
2022-07-08 10:15:32 -0500

    working on test to verfiy homepage url in first page

--- test/pageobjects/google.page.js
+++ test/pageobjects/google.page.js
@@ -5,7 +5,8 @@ class Google{
 get searchInput(){ return $('input.gLFyf.gsfi')}
 get googleSearchButton() { return $(".CqAVzb input.gNO89b")}
 get parentSearchResult(){ return $('#search')}
-get childSearchResult(){ return this.parentSearchResult.$$('#search .g.Ww4FFb')}
+get childSearchResult(){ return this.parentSearchResult.$$('#search .g.Ww4FFb')}// maybe change to 
+get childSearchResultsLink(){ return this.parentSearchResult.$$('#search .yuRUbf a')}// maybe change to 
 get searchResultsNavigation(){ return $('#hdtb')}
 searchResultsNavigationItem(index){ return $(`#hdtb .hdtb-mitem:nth-child(${index})`)}
 get googleLogo(){ return $("img[alt='Google']")}
@@ -18,7 +19,7 @@ async enterSearchTerm (text){
     await this.searchInput.click();
     await this.searchInput.setValue(text)
 }
-
+s
 async clickGoogleSearchButton(){
     await this.googleSearchButton.waitForDisplayed()
     await this.googleSearchButton.click()
@@ -28,5 +29,22 @@ async clickGoogleLogo(){
     await this.googleLogo.waitForDisplayed()
     await this.googleLogo.click()
 }
+
+async checkSearchReturnHcaLinks(){
+    const homepageUrl = 'https://hcahealthcare.com/'
+   
+//    const hcaLinks = await this.childSearchResultsLink.some( async childResultLink =>{
+   
+//       if( childResultLink.getAttribute('href') === homepageUrl){
+//          console.log('hca is found in first page')
+//          return true
+//       }
+//     })
+//     return await hcaLinks
+const hcaLinks = (links)=>links.getAttribute('href') === homepageUrl
+await this.childSearchResultsLink.some(hcaLinks)
 }
+//await this.childSearchResultsLink.getAttribute('href').includes(homepageUrl)
+}
+
 module.exports = new Google()
--- test/specs/hcaGoogleSearch.test.js
+++ test/specs/hcaGoogleSearch.test.js
@@ -1,7 +1,13 @@
- googlePage = require("../pageobjects/google.page");
+const googlePage = require("../pageobjects/google.page");
+
+ 
 
 describe('Hca Google search', function (){
     it('The official HCA Healthcare website is returned within the first three returned results', async ()=>{
-        await browser.url('https://google.com/')
+        await browser.url('https://google.com/');
+        await googlePage.enterSearchTerm('hca');
+        await googlePage.clickGoogleSearchButton();
+        console.log('This is the result of the method: ',await googlePage.checkSearchReturnHcaLinks());
+        assert.equal(true, await googlePage.checkSearchReturnHcaLinks(),'First page did not contain hca homepage')
     })
 })
